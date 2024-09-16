cccecf6af93374c9b802b9237117a4a35885847d
StarrDeFi <estheressienjames2020@gmail.com>
2024-09-16 14:00:14 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("skrc","skrc721")
+        ERC721("wrc","wrc721")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
