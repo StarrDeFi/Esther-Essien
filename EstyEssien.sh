6b192e780ec243f7803d0c66695ee7ebbb8c4c00
StarrDeFi <estheressienjames2020@gmail.com>
2024-09-16 13:58:30 +0100

    Create EstyEssien.sh

---
+++ EstyEssien.sh
@@ -0,0 +1,7 @@
+echo "# Esther-Essien" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/StarrDeFi/Esther-Essien.git
+git push -u origin main
